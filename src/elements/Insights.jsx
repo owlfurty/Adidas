52fa6ae5a0369a22ff7d961c8c186be228d4e26e
Mustafa Cetin <mustafacetin@Mustafas-MacBook-Pro.local>
2020-06-04 19:26:51 +0200

    working insights

--- src/elements/Insights.jsx
+++ src/elements/Insights.jsx
@@ -1,12 +1,82 @@
 import React, { Component } from "react";
 
 class Insights extends Component{
+
+    state = {entries: [], isLoading:true}
+
+    getEntries = () => {
+        fetch('https://cdn.contentstack.io/v3/content_types/blog_post/entries?environment=development&locale=en-us', {
+            method:'get',
+            mode:'cors',
+            headers:{
+                api_key:'bltb9eff0ec0532965e',
+                access_token:'csbcb89082a35b960cf9d10e11',
+                Accept: "*/*"
+            }
+        })
+        .then((response)=> {
+            return response.json()
+        })
+        .then((json) => {
+            console.log(json)
+            this.setState({entries:json.entries, isLoading:false})
+        })
+    }
+
+    componentDidMount(){
+        this.getEntries()
+    }
     render(){
-        let
-        category = 'Insights',
+        const { entries, isLoading } = this.state;
+        
+        if ( isLoading ) {
+            return null;
+        }
+        let category = 'Insights',
         categoryName = 'Category Name',
         blogTitle = 'Blog title comes here',
         blogText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla suscipit velit elit. Aliquam blandit rhoncus fermentum. Donec mollis vehicula arcu quis interdum. Nullam euismod volutpat porttitor.';
+
+        let componentsList = entries.map((blogPost) => {
+
+            if ( blogPost.hero_image !== null){
+
+                let img_link = blogPost.hero_image.url
+
+                return (
+                    <div className="blog-item">
+                        <div className="image-holder">
+                            <img src={img_link} alt=""/>
+                        </div>
+                        <div className="content-holder">
+                            <p className="date">{blogPost.date}</p>
+                            <p className="author">{blogPost.author[0]} <span>commercetools</span> </p>
+                            <h3 className="category-name">{categoryName}</h3>
+                            <h2 className="blog-title">{blogPost.title}</h2>
+                            <p className="blog-text">{blogPost.summary}</p>
+                        </div>
+                    </div>
+                )
+
+            } else {
+
+                return (
+                    <div className="blog-item">
+                        <div className="image-holder">
+                            <img src="https://source.unsplash.com/user/dulceylima/930x930" alt=""/>
+                        </div>
+                        <div className="content-holder">
+                            <p className="date">{blogPost.date}</p>
+                            <p className="author">{blogPost.author[0]} <span>commercetools</span> </p>
+                            <h3 className="category-name">{categoryName}</h3>
+                            <h2 className="blog-title">{blogPost.title}</h2>
+                            <p className="blog-text">{blogPost.summary}</p>
+                        </div>
+                    </div>
+                )
+            }
+        })
+       
         return(
             <React.Fragment>
                 <div className="insights-wrapper">
@@ -23,58 +93,7 @@ class Insights extends Component{
 
                             <div className="col-12">
                                 <div className="insights-inner inner">
-                                
-                                    <div className="blog-item">
-                                        <div className="image-holder">
-                                            <img src="https://source.unsplash.com/user/dulceylima/960x960" alt=""/>
-                                        </div>
-                                        <div className="content-holder">
-                                            <p className="date">may 18, 2020</p>
-                                            <p className="author">Kelly Goetsch <span>commercetools</span> </p>
-                                            <h3 className="category-name">{categoryName}</h3>
-                                            <h2 className="blog-title">{blogTitle}</h2>
-                                            <p className="blog-text">{blogText}</p>
-                                        </div>
-                                    </div>
-
-                                    <div className="blog-item">
-                                        <div className="image-holder">
-                                            <img src="https://source.unsplash.com/user/dulceylima/920x920" alt=""/>
-                                        </div>
-                                        <div className="content-holder">
-                                            <p className="date">may 18, 2020</p>
-                                            <p className="author">Kelly Goetsch <span>commercetools</span> </p>
-                                            <h3 className="category-name">{categoryName}</h3>
-                                            <h2 className="blog-title">{blogTitle}</h2>
-                                            <p className="blog-text">{blogText}</p>
-                                        </div>
-                                    </div>
-
-                                    <div className="blog-item">
-                                        <div className="image-holder">
-                                            <img src="https://source.unsplash.com/user/dulceylima/980x980" alt=""/>
-                                        </div>
-                                        <div className="content-holder">
-                                            <p className="date">may 18, 2020</p>
-                                            <p className="author">Kelly Goetsch <span>commercetools</span> </p>
-                                            <h3 className="category-name">{categoryName}</h3>
-                                            <h2 className="blog-title">{blogTitle}</h2>
-                                            <p className="blog-text">{blogText}</p>
-                                        </div>
-                                    </div>
-
-                                    <div className="blog-item">
-                                        <div className="image-holder">
-                                            <img src="https://source.unsplash.com/user/dulceylima/930x930" alt=""/>
-                                        </div>
-                                        <div className="content-holder">
-                                            <p className="date">may 18, 2020</p>
-                                            <p className="author">Kelly Goetsch <span>commercetools</span> </p>
-                                            <h3 className="category-name">{categoryName}</h3>
-                                            <h2 className="blog-title">{blogTitle}</h2>
-                                            <p className="blog-text">{blogText}</p>
-                                        </div>
-                                    </div>
+                                    {componentsList}
                                 </div>
                             </div>
                         </div>
