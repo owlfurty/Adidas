0c05aa664de5744ca4899ff16cbfc215fb483fea
Fehim Ismailov <fehim.ismailov@gmail.com>
2020-06-16 00:36:32 +0200

    Add manifesto contentstack integration

--- src/elements/ManifestoComp.jsx
+++ src/elements/ManifestoComp.jsx
@@ -1,9 +1,44 @@
 import React, { Component } from "react";
 
 class ManifestoComp extends Component{
+
+    constructor(){
+        super()
+
+        this.state = { isLoading : true, manifesto: null}
+    }
+
+    componentDidMount(){
+        this.getManifest()
+    }
+
+    getManifest = () => {
+        fetch('https://cdn.contentstack.io/v3/content_types/manifesto/entries?environment=development&locale=en-us', {
+            method:'get',
+            mode:'cors',
+            headers:{
+                api_key:'bltb9eff0ec0532965e',
+                access_token:'csbcb89082a35b960cf9d10e11',
+                Accept: "*/*"
+            }
+        })
+        .then((response)=> {
+            return response.json()
+        })
+        .then((json) => {
+            this.setState({manifesto:json.entries[0], isLoading:false})
+        })
+    }
+    
+    
     render(){
-        let
-        category = 'Manifesto'
+        const { isLoading, manifesto} = this.state;
+        let category = 'Manifesto'
+
+        if(isLoading){
+            return null
+        }
+
         return(
             <React.Fragment>
                 <div className="manifesto-wrapper">
@@ -14,7 +49,7 @@ class ManifestoComp extends Component{
                                 <div className="manifesto-inner inner">
                                     <div className="section-title">
                                         <h3 className="category">{category}</h3>
-                                        <h6 className="quote-title"> ---------- quoteTitle ----------</h6>
+                                        <h6 className="quote-title"> {manifesto.title}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -23,7 +58,7 @@ class ManifestoComp extends Component{
                     <div className="container-desktop">
                         <div className="quote-thumb">
                             <p className="text-white text-italic">
-                                ---------- quoteThumb ----------
+                                {manifesto.quote_thumb}
                             </p>
                         </div>
                     </div>
@@ -32,13 +67,11 @@ class ManifestoComp extends Component{
                             <div className="col-lg-8 col-md-12">
                                 <div className="paragraph">
                                     <p className="summary">
-                                    ---------- KisaParagraf ----------
+                                    {manifesto.summary}
                                     </p>
-                                    <p className="body-text">
-                                    ---------- UzunParagraf ----------
-                                    </p>
+                                    <div className="body-text" dangerouslySetInnerHTML={{ __html: manifesto.full_text} } />
                                     <a className="text-link" href="/">
-                                    ---------- HepsiniOkuLinki ----------
+                                    {manifesto.read_more}
                                     </a>
                                 </div>
                             </div>
