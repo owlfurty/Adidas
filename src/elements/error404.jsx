5d757cd22ebf89f53a445704b13c1a659c30c8ae
Mustafa Cetin <mustafacetin@Mustafas-MacBook-Pro.local>
2020-06-22 21:52:26 +0200

    404

--- src/elements/error404.jsx
+++ src/elements/error404.jsx
@@ -1,33 +1,39 @@
-import React, { Component } from 'react';
-import Header from "../component/header/HeaderFour";
+import React, { Component } from "react";
+import PageHelmet from "../component/common/Helmet";
 import ScrollToTop from 'react-scroll-up';
+import Header from "../component/header/Header";
+import Footer from "../component/footer/Footer";
 import { FiChevronUp } from "react-icons/fi";
-import Footer from "../component/footer/FooterTwo";
+import Getintouch from "../elements/Getintouch"
+class error404 extends Component {
 
- class error404 extends Component {
-    
     render() {
         return (
-            <>
-                <Header headerPosition="header--transparent" color="color-white" logo="logo-light" />
-                {/* Start Page Error  */}
-                <div className="error-page-inner bg_color--4">
-                    <div className="container">
+            <React.Fragment>
+                <PageHelmet pageTitle='About MACH Alliance' />
+                <Header headertransparent="header--transparent" colorblack="color--black" logoname="logo.png" />
+
+                {/* Start Breadcrump Area */}
+                <div className="sub-hero rn-page-title-area pt--120 bg_image" style={{ backgroundImage: `url(/assets/images/hero-images/404.jpg)` }} data-black-overlay="3">
+                    <div className="container-desktop position-relative">
                         <div className="row">
-                            <div className="col-lg-12">
+                            <div className="col-lg-12 pt--180 text-right">
                                 <div className="inner">
-                                    <h1 className="title theme-gradient">404!</h1>
-                                    <h3 className="sub-title">Page not found</h3>
-                                    <span>The page you were looking for could not be found.</span>
-                                    <div className="error-button">
-                                        <a className="rn-button-style--2 btn-solid" href="/">Back To Homepage</a>
-                                    </div>
+                                    <h1 className="title">404</h1>
+                                    <h1 className="title">Something is wrong</h1>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                {/* End Page Error  */}
+                {/* End Breadcrump Area */}
+
+                
+                {/* Start Getintouch Area */}
+                <div className="container-desktop getintouch-area getintouch-position-top">
+                    <Getintouch />
+                </div>
+                {/* End Getintouch Area */}
 
                 {/* Start Back To Top */}
                 <div className="backto-top">
@@ -36,10 +42,15 @@ import Footer from "../component/footer/FooterTwo";
                     </ScrollToTop>
                 </div>
                 {/* End Back To Top */}
-                
-                <Footer /> 
-            </>
+
+                <Footer />
+
+            </React.Fragment>
         )
     }
 }
 export default error404;
+
+
+
+
