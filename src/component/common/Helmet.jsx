f2d78dfb6e4e9b730a66d420f0af3f264d5363a0
Fehim Ismailov <fehim.ismailov@gmail.com>
2020-07-14 17:35:09 +0200

    testing og prefix

--- src/component/common/Helmet.jsx
+++ src/component/common/Helmet.jsx
@@ -6,11 +6,11 @@ class PageHelmet extends Component{
         return(
             <React.Fragment>
                 <Helmet>
-                    <title>{this.props.pageTitle} || Enterprise MACHified.</title>
-                    <meta name="description" content="MACH – The massive, monolithic enterprise software platforms that dominate today keep delivering slightly ‘faster horses’ to their customers." />
                     <meta property="og:title" content={this.props.ogTitle || ""} data-react-helmet="true"/>
                     <meta property="og:description" content={this.props.ogDescription || ""} data-react-helmet="true"/>
                     <meta property="og:image" content={this.props.ogImage || ""} data-react-helmet="true"/>
+                    <title>{this.props.pageTitle} || Enterprise MACHified.</title>
+                    <meta name="description" content="MACH – The massive, monolithic enterprise software platforms that dominate today keep delivering slightly ‘faster horses’ to their customers." />
                 </Helmet>
             </React.Fragment>
         )
